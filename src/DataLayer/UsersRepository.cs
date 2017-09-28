b0b68731ba88b8ac4c6aa7acf0739531c4ea5b76
CassandraDied <cassandradied@gmail.com>
2017-09-28 16:19:50 +0300

    Implemented Users repo.

--- src/DataLayer/UsersRepository.cs
+++ src/DataLayer/UsersRepository.cs
@@ -2,19 +2,47 @@
 using System.Threading.Tasks;
 using DataLayer.Contract;
 using DataLayer.Contract.Models;
+using System;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Linq;
 
 namespace DataLayer
 {
     public class UsersRepository : IUsersRepository
     {
-        public Task<User> FindUser(string id)
+        private static HttpClient _client = new HttpClient(); // TODO use IoC
+        private const string RESOURCE = "users";
+        private const int PAGE_SIZE = 5;
+
+
+        public async Task<User> FindUser(string userId)
         {
-            throw new System.NotImplementedException();
+            if (string.IsNullOrEmpty(userId))
+                throw new ArgumentNullException(nameof(userId));
+
+            var req = Constants.FakeDataHost + RESOURCE + "/" + userId;
+            return await GetOne(req);
         }
 
-        public Task<IEnumerable<User>> GetUsers(int page)
+        public async Task<IEnumerable<User>> GetUsers(int page)
         {
-            throw new System.NotImplementedException();
+            var req = Constants.FakeDataHost + RESOURCE;
+            return await GetMany(req, page);
+        }
+
+        private async Task<IEnumerable<User>> GetMany(string req, int page)
+        {
+            var resp = await _client.GetStringAsync(req);
+
+            var deserialized = JsonConvert.DeserializeObject<IEnumerable<User>>(resp);
+            return deserialized.Skip(page * PAGE_SIZE);
+        }
+
+        private async Task<User> GetOne(string req)
+        {
+            var resp = await _client.GetStringAsync(req);
+            return JsonConvert.DeserializeObject<User>(resp);
         }
     }
 }
