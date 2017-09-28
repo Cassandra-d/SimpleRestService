e126fd3ed60273dfd720c3977cb79889fb9c16d7
CassandraDied <cassandradied@gmail.com>
2017-09-28 16:12:57 +0300

    Complete implementation AlbumsRepo.

--- src/DataLayer/AlbumsRepository.cs
+++ src/DataLayer/AlbumsRepository.cs
@@ -5,35 +5,67 @@ using DataLayer.Contract.Models;
 using System.Net.Http;
 using System;
 using Newtonsoft.Json;
+using System.Linq;
 
 namespace DataLayer
 {
     public class AlbumsRepository : IAlbumsRepository
     {
         private static HttpClient _client = new HttpClient(); // TODO use IoC
+        private const string RESOURCE = "albums";
+        private const int PAGE_SIZE = 5;
 
         public async Task<Album> FindAlbum(string albumId)
         {
             if (string.IsNullOrEmpty(albumId))
                 throw new ArgumentNullException(nameof(albumId));
 
-            var req = Constants.FakeDataHost + "albums/" + albumId;
+            var req = Constants.FakeDataHost + RESOURCE + "/" + albumId;
+            return await GetOne(req);
+        }
+
+        public async Task<IEnumerable<Album>> GetAlbums(int page)
+        {
+            // well, the service does not support paging or navigation, so we have to retrieve everything
+            // I don't see any reason why I should implement any optimisations here like caching or something similar, as it is not a programming problem I need to solve, but data storage
+
+            var req = Constants.FakeDataHost + RESOURCE;
+            return await GetMany(req, page);
+        }
+
+        public async Task<IEnumerable<Album>> GetAlbumsForUser(string userId, int page)
+        {
+            if (string.IsNullOrEmpty(userId))
+                throw new ArgumentNullException(nameof(userId));
+
+            // not nice situation here: we want to work only with albums here, but have to work with users too
+            // the reason why it is here and not in users repo is that any developer that would want to look at how we retrieve albums for user will probably look at this repo first
+
+            string usersResource = "users";
+            var req = Constants.FakeDataHost + usersResource + "/" + userId + "/" + RESOURCE;
+
+            return await GetMany(req, page);
+        }
+
+        private async Task<IEnumerable<Album>> GetMany(string req, int page)
+        {
+            var resp = await _client.GetStringAsync(req);
+
+            if (string.IsNullOrEmpty(resp))
+                return Enumerable.Empty<Album>();
+
+            var deserialized = JsonConvert.DeserializeObject<IEnumerable<Album>>(resp);
+            return deserialized.Skip(page * PAGE_SIZE).ToArray();
+        }
+
+        private async Task<Album> GetOne(string req)
+        {
             var resp = await _client.GetStringAsync(req);
 
             if (string.IsNullOrEmpty(resp))
                 return null;
 
             return JsonConvert.DeserializeObject<Album>(resp);
-        }
-
-        public Task<IEnumerable<Album>> GetAlbums(int page)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public Task<IEnumerable<Album>> GetAlbumsForUser(string userId, int page)
-        {
-            throw new System.NotImplementedException();
         }
     }
 }
